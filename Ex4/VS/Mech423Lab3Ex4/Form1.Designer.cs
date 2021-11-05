23ce3d6c26dbb954c93296fd01362a53bfd1c96d
t-guan <56769857+t-guan@users.noreply.github.com>
2021-11-04 18:02:01 -0700

    Chart Integration Completed, has one single test case inside which is commented out inside ConBut_Mouseclick

---
+++ Ex4/VS/Mech423Lab3Ex4/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace Mech423Lab3Ex4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.ConBut = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.PosChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.VelChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.PlotBut = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PosChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VelChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ConBut
+            // 
+            this.ConBut.Location = new System.Drawing.Point(163, 37);
+            this.ConBut.Name = "ConBut";
+            this.ConBut.Size = new System.Drawing.Size(128, 43);
+            this.ConBut.TabIndex = 0;
+            this.ConBut.Text = "Connect";
+            this.ConBut.UseVisualStyleBackColor = true;
+            this.ConBut.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ConBut_MouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // PosChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.PosChart.ChartAreas.Add(chartArea1);
+            this.PosChart.ImeMode = System.Windows.Forms.ImeMode.Off;
+            legend1.Name = "Legend1";
+            this.PosChart.Legends.Add(legend1);
+            this.PosChart.Location = new System.Drawing.Point(47, 103);
+            this.PosChart.Name = "PosChart";
+            this.PosChart.Size = new System.Drawing.Size(1285, 1091);
+            this.PosChart.TabIndex = 2;
+            this.PosChart.Text = "PositionChart";
+            // 
+            // VelChart
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.VelChart.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.VelChart.Legends.Add(legend2);
+            this.VelChart.Location = new System.Drawing.Point(1338, 103);
+            this.VelChart.Name = "VelChart";
+            this.VelChart.Size = new System.Drawing.Size(1297, 1091);
+            this.VelChart.TabIndex = 3;
+            this.VelChart.Text = "VelocityChart";
+            // 
+            // PlotBut
+            // 
+            this.PlotBut.Location = new System.Drawing.Point(391, 37);
+            this.PlotBut.Name = "PlotBut";
+            this.PlotBut.Size = new System.Drawing.Size(143, 43);
+            this.PlotBut.TabIndex = 4;
+            this.PlotBut.Text = "Plot";
+            this.PlotBut.UseVisualStyleBackColor = true;
+            this.PlotBut.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PlotBut_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2915, 1412);
+            this.Controls.Add(this.PlotBut);
+            this.Controls.Add(this.VelChart);
+            this.Controls.Add(this.PosChart);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ConBut);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PosChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VelChart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button ConBut;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart PosChart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart VelChart;
+        private System.Windows.Forms.Button PlotBut;
+    }
+}
+
---
+++ Ex4/VS/Mech423Lab3Ex4/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Mech423Lab3Ex4
+{
+    public partial class Form1 : Form
+    {
+        ConcurrentQueue<Int32> Posvalues = new ConcurrentQueue<Int32>();
+        ConcurrentQueue<Int32> Velvalues = new ConcurrentQueue<Int32>();
+        int x = 0;
+        int pval = 0;
+        int vval = 0;
+        Series posdata = new Series();
+        Series veldata = new Series();
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Position Chart Init
+            PosChart.Series.Add(posdata);
+            posdata.Name = "Position Data";
+            posdata.ChartType = SeriesChartType.Line;
+            posdata.BorderWidth = 2;
+            posdata.Color = Color.Purple;
+            posdata.XValueType = ChartValueType.Int32;
+            posdata.YValueType = ChartValueType.Int32;
+            //Velocity Chart Init
+            VelChart.Series.Add(veldata);
+            veldata.Name = "Velocity Data";
+            veldata.ChartType = SeriesChartType.Line;
+            veldata.BorderWidth = 2;
+            veldata.Color = Color.Purple;
+            veldata.XValueType = ChartValueType.Int32;
+            veldata.YValueType = ChartValueType.Int32;
+            //Timer Initialization
+            timer1.Interval = 100;
+            timer1.Tick += new EventHandler(timer1_Tick);
+            //Serial Port Init
+            serialPort1.PortName = "COM5";
+
+        }
+        private void ConBut_MouseClick(object sender, MouseEventArgs e)
+        {
+/*            //For Testing purposes only
+           for (int i = 0; i < 50000; i++)
+            {
+                int p = rnd.Next(1, 100);
+                Posvalues.Enqueue(p);
+                int v = rnd.Next(1, 100);
+                Velvalues.Enqueue(v);
+            }
+            MessageBox.Show("Values Generated", "Notice");*/
+
+        }
+        private void UpdateSeries()
+        {
+            if (x < Posvalues.Count)
+            {
+                Posvalues.TryDequeue(out pval);
+                Velvalues.TryDequeue(out vval);
+                if (posdata.Points.Count() > 100) posdata.Points.RemoveAt(0);
+                if (veldata.Points.Count() > 100) veldata.Points.RemoveAt(0);
+                posdata.Points.AddXY(x, pval);
+                veldata.Points.AddXY(x, vval);
+                PosChart.ResetAutoValues();
+                VelChart.ResetAutoValues();
+                x++;
+            }
+            else
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("No More Data", "Error");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateSeries();
+        }
+
+        private void PlotBut_MouseClick(object sender, MouseEventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+    }
+}
