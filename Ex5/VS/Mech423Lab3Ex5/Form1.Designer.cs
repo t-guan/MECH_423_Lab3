c483856f90b735ba7b1a0878930dea09ae276369
Joseph Castillo <62954657+jcstlo@users.noreply.github.com>
2021-11-06 20:43:41 -0700

    Copy Ex4 template to Ex5

---
+++ Ex5/VS/Mech423Lab3Ex5/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿
+namespace Mech423Lab3Ex5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.ConBut = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.PosChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.VelChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.SliCon = new System.Windows.Forms.TrackBar();
+            this.SliConCheck = new System.Windows.Forms.CheckBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.posBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.velBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.VelCountBox = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.pwmrotchart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.PosChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VelChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SliCon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pwmrotchart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ConBut
+            // 
+            this.ConBut.Location = new System.Drawing.Point(164, 37);
+            this.ConBut.Margin = new System.Windows.Forms.Padding(4);
+            this.ConBut.Name = "ConBut";
+            this.ConBut.Size = new System.Drawing.Size(128, 42);
+            this.ConBut.TabIndex = 0;
+            this.ConBut.Text = "Connect";
+            this.ConBut.UseVisualStyleBackColor = true;
+            this.ConBut.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ConBut_MouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 37);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // PosChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.PosChart.ChartAreas.Add(chartArea1);
+            this.PosChart.ImeMode = System.Windows.Forms.ImeMode.Off;
+            legend1.Name = "Legend1";
+            this.PosChart.Legends.Add(legend1);
+            this.PosChart.Location = new System.Drawing.Point(48, 104);
+            this.PosChart.Margin = new System.Windows.Forms.Padding(4);
+            this.PosChart.Name = "PosChart";
+            this.PosChart.Size = new System.Drawing.Size(1284, 1090);
+            this.PosChart.TabIndex = 2;
+            this.PosChart.Text = "PositionChart";
+            // 
+            // VelChart
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.VelChart.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.VelChart.Legends.Add(legend2);
+            this.VelChart.Location = new System.Drawing.Point(1338, 104);
+            this.VelChart.Margin = new System.Windows.Forms.Padding(4);
+            this.VelChart.Name = "VelChart";
+            this.VelChart.Size = new System.Drawing.Size(1296, 1090);
+            this.VelChart.TabIndex = 3;
+            this.VelChart.Text = "VelocityChart";
+            // 
+            // SliCon
+            // 
+            this.SliCon.Location = new System.Drawing.Point(48, 1361);
+            this.SliCon.Margin = new System.Windows.Forms.Padding(6);
+            this.SliCon.Maximum = 100;
+            this.SliCon.Name = "SliCon";
+            this.SliCon.Size = new System.Drawing.Size(800, 90);
+            this.SliCon.TabIndex = 5;
+            this.SliCon.Value = 50;
+            // 
+            // SliConCheck
+            // 
+            this.SliConCheck.AutoSize = true;
+            this.SliConCheck.Location = new System.Drawing.Point(57, 1341);
+            this.SliConCheck.Margin = new System.Windows.Forms.Padding(6);
+            this.SliConCheck.Name = "SliConCheck";
+            this.SliConCheck.Size = new System.Drawing.Size(235, 29);
+            this.SliConCheck.TabIndex = 6;
+            this.SliConCheck.Text = "Slider Motor Control";
+            this.SliConCheck.UseVisualStyleBackColor = true;
+            // 
+            // posBox
+            // 
+            this.posBox.Location = new System.Drawing.Point(164, 1204);
+            this.posBox.Name = "posBox";
+            this.posBox.Size = new System.Drawing.Size(100, 31);
+            this.posBox.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 1207);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Position:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1333, 1210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Velocity:";
+            // 
+            // velBox
+            // 
+            this.velBox.Location = new System.Drawing.Point(1445, 1207);
+            this.velBox.Name = "velBox";
+            this.velBox.Size = new System.Drawing.Size(100, 31);
+            this.velBox.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1333, 1255);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(286, 25);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Velocity Counts Per Second:";
+            // 
+            // VelCountBox
+            // 
+            this.VelCountBox.Location = new System.Drawing.Point(1625, 1252);
+            this.VelCountBox.Name = "VelCountBox";
+            this.VelCountBox.Size = new System.Drawing.Size(100, 31);
+            this.VelCountBox.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(56, 1269);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 31);
+            this.textBox2.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(164, 1269);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 31);
+            this.textBox3.TabIndex = 15;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(278, 1269);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 31);
+            this.textBox4.TabIndex = 16;
+            // 
+            // pwmrotchart
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.pwmrotchart.ChartAreas.Add(chartArea3);
+            this.pwmrotchart.ImeMode = System.Windows.Forms.ImeMode.Off;
+            legend3.Name = "Legend1";
+            this.pwmrotchart.Legends.Add(legend3);
+            this.pwmrotchart.Location = new System.Drawing.Point(993, 1341);
+            this.pwmrotchart.Margin = new System.Windows.Forms.Padding(4);
+            this.pwmrotchart.Name = "pwmrotchart";
+            this.pwmrotchart.Size = new System.Drawing.Size(1226, 425);
+            this.pwmrotchart.TabIndex = 17;
+            this.pwmrotchart.Text = "PWMRPMChart";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2253, 1793);
+            this.Controls.Add(this.pwmrotchart);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.VelCountBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.velBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.posBox);
+            this.Controls.Add(this.SliConCheck);
+            this.Controls.Add(this.SliCon);
+            this.Controls.Add(this.VelChart);
+            this.Controls.Add(this.PosChart);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ConBut);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PosChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VelChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SliCon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pwmrotchart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button ConBut;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart PosChart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart VelChart;
+        private System.Windows.Forms.TrackBar SliCon;
+        private System.Windows.Forms.CheckBox SliConCheck;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.TextBox posBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox velBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox VelCountBox;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart pwmrotchart;
+    }
+}
+
---
+++ Ex5/VS/Mech423Lab3Ex5/Form1.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Mech423Lab3Ex5
+{
+    public partial class Form1 : Form
+    {
+        //Collect UART data
+        ConcurrentQueue<Int32> databyte = new ConcurrentQueue<Int32>();
+        //Encoder Count storage
+        ConcurrentQueue<Int32> encoderUpCounts = new ConcurrentQueue<Int32>();
+        ConcurrentQueue<Int32> encoderDownCounts = new ConcurrentQueue<Int32>();
+        //Direction and PWM Bytes for DC motor Control
+        ConcurrentQueue<Int32> DirectionByte = new ConcurrentQueue<Int32>();
+        ConcurrentQueue<Int32> PWMByte = new ConcurrentQueue<Int32>();
+        int x = 0;
+        int value = 0;
+        int counter = 0;
+        int usum = 0;
+        int dsum = 0;
+        int avg = 45;
+        int halfticks;
+        int bytesToRead = 0;
+        int is255 = 0;
+        int freq = 1000;
+        double position = 0.0;
+        double circ = 0.523;
+        private static int dirval;
+        private static int pwmval;
+        private static int sliderticks = 8;
+        private int prevsliderpos = 999;
+
+        //The divisor for velocity
+        double timeDiff = 0.6;
+        Series posdata = new Series();
+        Series veldata = new Series();
+        Series pwmdata = new Series();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Position Chart Init
+            PosChart.Series.Add(posdata);
+            posdata.Name = "Position Data";
+            posdata.ChartType = SeriesChartType.Line;
+            posdata.BorderWidth = 2;
+            posdata.Color = Color.Purple;
+            posdata.XValueType = ChartValueType.Double;
+            posdata.YValueType = ChartValueType.Double;
+            //Velocity Chart Init
+            VelChart.Series.Add(veldata);
+            veldata.Name = "Velocity Data";
+            veldata.ChartType = SeriesChartType.Line;
+            veldata.BorderWidth = 2;
+            veldata.Color = Color.Purple;
+            veldata.XValueType = ChartValueType.Double;
+            veldata.YValueType = ChartValueType.Double;
+            //PWM RPM Chart Init
+            pwmrotchart.Series.Add(pwmdata);
+            pwmdata.Name = "PWM VS Rotation";
+            pwmdata.ChartType = SeriesChartType.Point;
+            pwmdata.BorderWidth = 2;
+            pwmdata.Color = Color.Purple;
+            pwmdata.XValueType = ChartValueType.Int32;
+            pwmdata.YValueType = ChartValueType.Double;
+            //Timer Initialization
+            timer1.Interval = 1000;
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Enabled = true;
+            timer2.Interval = 1000;
+            timer2.Tick += new EventHandler(timer2_Tick);
+            timer2.Enabled = true;
+            //Serial Port Init
+            serialPort1.PortName = "COM5";
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            //Variable Inits
+            sliderticks = SliCon.Maximum;
+            halfticks = sliderticks / 2;
+        }
+        private void ConBut_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            if (serialPort1.IsOpen == false)
+            {
+                serialPort1.Open();
+                ConBut.Text = "Disconnect";
+            }
+            if (serialPort1.IsOpen == true)
+            {
+                serialPort1.Close();
+                ConBut.Text = "Connect";
+            }
+        }
+        private void SumConverter(int upc, int doc)
+        {
+
+            double velocityCPS;
+            double velocityRPM;
+            velocityCPS = ((double)upc - (double)doc) / timeDiff;
+            VelCountBox.Text = velocityCPS.ToString();
+            velocityRPM = (velocityCPS * 60.0 / (20.4 * 12.0));
+            position = position + ((velocityRPM * 8 * 3.14) / 60) * timeDiff;
+            //position = (position + ((velocityCPS * 0.25 * 4) / (20.4 * 48.0)));
+            if (posdata.Points.Count() > 100) posdata.Points.RemoveAt(0);
+            if (veldata.Points.Count() > 100) veldata.Points.RemoveAt(0);
+            if (pwmdata.Points.Count() > 100) pwmdata.Points.RemoveAt(0);
+            posBox.Text = position.ToString();
+            velBox.Text = velocityRPM.ToString();
+            posdata.Points.AddXY(x, position);
+            veldata.Points.AddXY(x, velocityRPM);
+            pwmdata.Points.AddXY(pwmval, velocityCPS);
+            PosChart.ResetAutoValues();
+            VelChart.ResetAutoValues();
+            pwmrotchart.ResetAutoValues();
+        }
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            int newByte = 0;
+            bytesToRead = serialPort1.BytesToRead;
+            while (bytesToRead != 0)
+            {
+                newByte = serialPort1.ReadByte();
+                databyte.Enqueue(newByte);
+                bytesToRead = serialPort1.BytesToRead;
+            }
+        }
+        //Converter converts values, performs math, calls UpdateSeries, currently not in use
+        /*        private void Converter()
+                {
+                    double position = 0.0;
+                    double velocityCPS;
+                    double velocityRPM;
+                    int upcount;
+                    int downcount;
+                    if (encoderUpCounts.TryDequeue(out upcount) && encoderDownCounts.TryDequeue(out downcount))
+                    {
+                        textBox1.Text = upcount.ToString();
+                        textBox4.Text = downcount.ToString();
+                        //TODO: Perform RPM conversion
+                        velocityCPS = ((double)upcount - (double)downcount) / timeDiff;
+                        VelCountBox.Text = velocityCPS.ToString();
+                        velocityRPM = (velocityCPS * 60.0 / (20.4 * 12.0));
+                        position = (position + ((velocityCPS * 0.25 * 4) / (20.4 * 48.0)));
+                        if (posdata.Points.Count() > 100) posdata.Points.RemoveAt(0);
+                        if (veldata.Points.Count() > 100) veldata.Points.RemoveAt(0);
+                        posBox.Text = position.ToString();
+                        velBox.Text = velocityRPM.ToString();
+                        posdata.Points.AddXY(x, position);
+                        veldata.Points.AddXY(x, velocityRPM);
+                        PosChart.ResetAutoValues();
+                        VelChart.ResetAutoValues();
+                        x++;
+                    }
+
+                }*/
+        //Only collects data from UART, calls Converter, currently collecting only 3 data bytes as opposed to 5
+        //Can modify as required
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                int valfromq;
+                while (databyte.TryDequeue(out valfromq))
+                {
+                    switch (is255)
+                    {
+                        case 1:
+                            encoderUpCounts.Enqueue(valfromq);
+                            textBox2.Text = valfromq.ToString();
+                            is255++;
+                            break;
+                        case 2:
+                            encoderDownCounts.Enqueue(valfromq);
+                            textBox3.Text = valfromq.ToString();
+                            is255 = 0;
+                            break;
+                    }
+                    if (valfromq == 255)
+                    {
+                        is255 = 1;
+                    }
+                    counter++;
+                    if (counter > avg)
+                    {
+                        for (int i = 0; i < avg; i++)
+                        {
+                            encoderUpCounts.TryDequeue(out value);
+                            usum = usum + value;
+                        }
+                        usum = usum / avg;
+                        for (int i = 0; i < avg; i++)
+                        {
+                            encoderDownCounts.TryDequeue(out value);
+                            dsum = dsum + value;
+                        }
+                        dsum = dsum / avg;
+                        SumConverter(usum, dsum);
+                        counter = 0;
+                        usum = 0;
+                        dsum = 0;
+                    }
+                    // Converter();
+
+                }
+
+            }
+        }
+        private void PreparePackets(int direction, int pwm)
+        {
+            if (direction == 0 || direction == 1 || direction == 2)
+            {
+                PrepareDirPacket(direction);
+                PreparePWMPacket(pwm);
+                if (DirectionByte.Count == 5 && PWMByte.Count == 5)
+                {
+                    SendPackets();
+                }
+                else
+                {
+                    DestroyPackets();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid Direction, No Action Taken", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void PrepareDirPacket(int direction)
+        {
+            //Enqueue first byte
+            DirectionByte.Enqueue(255);
+            //Enqueue direction byte
+            DirectionByte.Enqueue(direction);
+            //Fill rest with Zeros
+            for (int i = 0; i < 3; i++)
+            {
+                DirectionByte.Enqueue(0);
+            }
+        }
+        private void DestroyPackets()
+        {
+            int garbage;
+            while (DirectionByte.Count > 0)
+            {
+                DirectionByte.TryDequeue(out garbage);
+            }
+            while (PWMByte.Count > 0)
+            {
+                PWMByte.TryDequeue(out garbage);
+            }
+        }
+        private void PreparePWMPacket(int pwm)
+        {
+            if (pwm > 100)
+            {
+                pwm = 65535;
+            }
+            else if (pwm < 0)
+            {
+                pwm = 0;
+            }
+            else
+            {
+                pwm = (int)((1 - (double)pwm / 100) * 65535);
+            }
+            ushort pwmnum16 = Convert.ToUInt16(pwm);
+            byte upperpwm = (byte)(pwmnum16 >> 8);
+            byte lowerpwm = (byte)(pwmnum16 & 0xff);
+            byte esc = 0;
+
+            if (upperpwm == 255 && lowerpwm == 255)
+            {
+                upperpwm = 0;
+                lowerpwm = 0;
+                esc = 3;
+            }
+            else if (upperpwm == 255 && lowerpwm != 255)
+            {
+                upperpwm = 0;
+                esc = 2;
+            }
+            else if (upperpwm != 255 && lowerpwm == 255)
+            {
+                lowerpwm = 0;
+                esc = 1;
+            }
+            else
+            {
+                esc = 0;
+            }
+
+            //Enqueue First Byte
+            PWMByte.Enqueue(255);
+            //Enqeue 0 for direction, so the direction is not chagned
+            PWMByte.Enqueue(0);
+            //Enqueue the Upper Byte first
+            PWMByte.Enqueue(upperpwm);
+            //Enqueue the lower byte
+            PWMByte.Enqueue(lowerpwm);
+            // Enqueue escape byte
+            PWMByte.Enqueue(esc);
+        }
+        private void SendPackets()
+        {
+            byte[] Dir_Byte = { 0, 0, 0, 0, 0 };
+            byte[] PWM_Byte = { 0, 0, 0, 0, 0 };
+            int value;
+            for (int i = 0; i < 5; i++)
+            {
+                DirectionByte.TryDequeue(out value);
+                Dir_Byte[i] = (byte)value;
+                PWMByte.TryDequeue(out value);
+                PWM_Byte[i] = (byte)value;
+            }
+            serialPort1.Write(Dir_Byte, 0, 5);
+            serialPort1.Write(PWM_Byte, 0, 5);
+
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int pwmscale = (int)((double)100 / halfticks);
+            if (SliConCheck.Checked)
+            {
+                int sliderpos = SliCon.Value;
+                if (sliderpos == halfticks)
+                {
+                    dirval = 0;
+                    pwmval = 0;
+                }
+                else if (sliderpos < halfticks)
+                {
+                    pwmval = (halfticks - sliderpos) * pwmscale;
+                    dirval = 2;
+
+                }
+                else if (sliderpos > halfticks)
+                {
+                    pwmval = (sliderpos - halfticks) * pwmscale;
+                    dirval = 1;
+                }
+
+                //Send the read values to PreparePackets
+                if ((sliderpos != prevsliderpos) && (prevsliderpos != 999))
+                {
+                    PreparePackets(dirval, pwmval);
+                }
+                else if (prevsliderpos == -999)
+                {
+                    PreparePackets(dirval, pwmval);
+                }
+                prevsliderpos = sliderpos;
+            }
+        }
+    }
+}
